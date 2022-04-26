53e9e7c311d5a532f7e55a9df36e3d408e0fcb83
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-27 02:05:33 +0600

    added mongoDB integration and method to load all products data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5050;
@@ -8,6 +9,39 @@ const port = process.env.PORT || 5050;
 app.use(cors());
 app.use(express.json());
 
+// ********************************
+// MongoDB integration
+const uri =
+    'mongodb+srv://kmrajibfaysal:N3nGnsw3sZjxGM1c@cluster0.yzknm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('MongoDB connection Successful');
+
+        const database = client.db('amazon-simple');
+        const productsCollection = database.collection('products');
+
+        const query = {};
+        const cursor = productsCollection.find(query);
+        const products = await cursor.toArray();
+
+        // database get operations
+        app.get('/products', async (req, res) => {
+            res.send(products);
+        });
+    } finally {
+        //
+    }
+}
+
+run().catch(console.dir);
+// ********************************
 // GET requests
 app.get('/', (req, res) => {
     res.send('Amazon simple server is running in port 5050');
